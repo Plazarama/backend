d4049b2183dd62f62ea0e5f13c757845215b663f
Alex <aletormat@gmail.com>
2015-11-23 12:28:21 +0000

    Migrated to EJS

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,12 @@ module.exports = function(app){
 	app.route('/')
 
 		.get(function(req, res){
-			res.render('index', {title: 'that'});
+			res.render('index', {title: 'EJS Template'});
 		});
 
+
+	app.route('/about')
+		.get(function(req, res){
+			res.render('about', {title: 'About page'});
+		});
 };
