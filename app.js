10f88f23782189b184ea4c51474723631102baf8
Alex <aletormat@gmail.com>
2015-11-23 12:00:58 +0000

    minor changes

--- app.js
+++ app.js
@@ -65,14 +65,4 @@ if (env === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 require('./routes/routes.js')(app);
