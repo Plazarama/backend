3c3ecee0ad22b745321ee43aaa51116b9d37b21f
Alex <aletormat@gmail.com>
2015-11-23 11:57:47 +0000

    New routes, express4config, and stuff

--- app.js
+++ app.js
@@ -1,48 +1,61 @@
 var express = require('express');
+var http = require('http');
 var path = require('path');
+
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+
+
+var config = require('./config/config');
+var secret = require('./config/secret');
+
+
+var mongoose = require('mongoose');
+var configDb = require('./config/database')(mongoose);
 
 var hbs = require('hbs');
 
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+var port = process.env.PORT || 3000;
 
-// view engine setup
-//app.set('view engine', 'jade');
+
+// view engine setup & static folder
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Config Express
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(session({secret: secret.sessionSecret}));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+
+// Run the app in the specific port
+app.listen(port);
+console.log('Express server listening on port '+port);
+
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+// Change for deploy
+var env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+  
+  app.use(morgan('dev'));
+  app.locals.pretty = true;
+
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
@@ -62,7 +75,4 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
-module.exports = app;
-
-var www = require('./bin/www');
+require('./routes/routes.js')(app);
---
+++ config/database.js
@@ -0,0 +1,15 @@
+module.exports = function(mongoose){
+	
+		var uristring = 
+		  process.env.MONGOLAB_URI || 
+		  process.env.MONGOHQ_URL || 
+		  'mongodb://localhost/p300';
+
+		mongoose.connect(uristring, function(err, res){
+			if(err){
+				console.log('ERROR: connecting to database. '+err);
+			}
+			else
+				console.log('Connected to Database.');
+		});
+};
