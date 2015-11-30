06335e4bb03c9942a41374be0cb856b3af9a3599
Alex <aletormat@gmail.com>
2015-11-30 11:44:14 +0000

    changed some things

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var http = require('http');
 var path = require('path');
+var app = express();
 
 var favicon = require('serve-favicon');
 var morgan = require('morgan');
@@ -10,19 +10,21 @@ var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 
-
 var config = require('./config/config');
 var secret = require('./config/secret');
-var seed = require('./config/seed');
-
 
 var mongoose = require('mongoose');
 var configDb = require('./config/database')(mongoose);
 
 
-var app = express();
-var port = process.env.PORT || 3000;
+// Run the server and socket.io
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
+var port = process.env.PORT || 3000;
+server.listen(port, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 // view engine setup & static folder
 
@@ -32,8 +34,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Config Express
-
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -45,17 +45,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
+
+// Authentication routes
 require('./config/passport')(passport);
 
-// Run the app in the specific port
-app.listen(port);
-console.log('Express server listening on port '+port);
 
-
-// error handlers
 
 // development error handler
-// will print stacktrace
 // Change for deploy
 var env = process.env.NODE_ENV || 'development';
 if (env === 'development') {
@@ -63,6 +59,8 @@ if (env === 'development') {
   app.use(morgan('dev'));
   app.locals.pretty = true;
 
+  //seed the db
+  var seed = require('./config/seed');
   seed.seedUser();
   seed.seedQuestions();
 
@@ -77,9 +75,9 @@ if (env === 'development') {
 
 // route middleware to add config variables to our response locals
 app.use(function(req, res, next) {
-	res.locals.title = 'Some title';
+    res.locals.title = 'Some title';
 
-    // continue doing what we were doing and go to the route
+
     next(); 
 });
 
